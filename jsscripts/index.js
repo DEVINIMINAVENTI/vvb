9fd1c9edc89b7f3d4458fd033ad44c3b28831e47
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-06-05 12:40:04 +0100

    Update index.js

--- jsscripts/index.js
+++ jsscripts/index.js
@@ -1,22 +1,25 @@
-const navToggler = document.querySelector(".nav-togglerb");
 
-navToggler.addEventListener('click', navToggle);
-
-function navToggle(){
-
-    const nav = document.querySelector(".sidebar ul");
-    nav.classList.toggle("sidebar");
-   
-}
 
 
 const navTogglera = document.querySelector(".nav-toggler");
 
-navTogglera.addEventListener('click', navToggles);
+navTogglera.addEventListener('click', navToggle);
 
-function navToggles(){
+function navToggle(){
 
     const nav = document.querySelector(".navbar ul");
     nav.classList.toggle("navbarbaa");
+   
+}
+
+
+const navTogglerb = document.querySelector(".buttonx");
+
+navTogglerb.addEventListener('click', navToggles);
+
+function navToggles(){
+
+    const nav = document.querySelector(".shops");
+    nav.classList.toggle("showshops");
    
 }
