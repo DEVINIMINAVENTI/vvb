ba0344bcb115e93fbfa4475f366631fe22b18492
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-06-08 13:58:36 +0100

    Add files via upload

---
+++ jsscripts/notification.js
@@ -0,0 +1,21 @@
+function showNotification(){
+    const notification = new Notification("Message from HANDOUTS STORE!", {
+        body: "We now have Handouts for Microbiology Second Semester",
+        icon: "https://www.handouts.store/images/header.jpeg"
+    });
+    notification.onclick = (e) => {
+        window.location.href = "https://www.handouts.store/handouts/100lvlmcb/";
+    };
+}
+
+console.log(Notification.permission);
+
+if (Notification.permission === "granted"){
+    showNotification();
+}  else if (Notification.permission !== "denied"){
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted"){
+            showNotification();
+        }
+    });
+}
